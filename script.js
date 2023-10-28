04ef81ebe72a0b67d0c17045353ca9ae7ae5700e
hyrumsdolan <79908304+hyrumsdolan@users.noreply.github.com>
2023-10-28 10:27:26 -0600

    make pretty

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// Assignment code here
-document.getElementById('generate').addEventListener('click', function() {
-  // Get the textarea element by its ID
-  const textarea = document.getElementById('password');
-  
-  // Set the value of the textarea
-  textarea.value = 'Hello, world!';
-});
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -104,9 +87,3 @@ function copyToClipboard() {
 
 // Attach event listener
 copyBtn.addEventListener("click", copyToClipboard);
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
